fd26d545727262fabf65b4fa5f1826091bb98fb1
Josh Parada <joshparada@Joshs-MacBook-Pro.local>
2021-11-03 14:59:58 -0500

    longestWord/shortestWord

--- exercises/strings/longestWord/longestWord.js
+++ exercises/strings/longestWord/longestWord.js
@@ -9,6 +9,17 @@
 
 function longestWord(string) {
   // This is your job. :)
+  var sepString = string.split(' ');
+  var longword = sepString[0];
+
+  console.log(sepString)
+
+  for (let i = 0; i < sepString.length; i++){
+    if(sepString[i].length > longword.length){
+      longword = sepString[i];
+    }
+  }
+  return longword;
 }
 
 if (require.main === module) {
@@ -16,6 +27,12 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+
+  console.log(longestWord('this is a bug') === 'this')
+  console.log(longestWord('slow trucks on the freeway') === 'freeway')
+  console.log(longestWord('it is raining') === 'raining')
+  console.log(longestWord('coding is fun') === 'coding')
+
 }
 
 module.exports = longestWord;
--- exercises/strings/shortestWord/shortestWord.js
+++ exercises/strings/shortestWord/shortestWord.js
@@ -9,6 +9,18 @@
 
 function shortestWord(string) {
   // This is your job. :)
+
+  var sepString = string.split(' ');
+  var shortword = sepString[0];
+
+  console.log(sepString)
+
+  for (let i = 0; i < sepString.length; i++){
+    if(sepString[i].length < shortword.length){
+      shortword = sepString[i];
+    }
+  }
+  return shortword;
 }
 
 if (require.main === module) {
@@ -16,6 +28,11 @@ if (require.main === module) {
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+
+  console.log(shortestWord('this is a bug') === 'a')
+  console.log(shortestWord('slow trucks on the freeway') === 'on')
+  console.log(shortestWord('its raining') === 'its')
+  console.log(shortestWord('coding is fun') === 'is')
 }
 
 module.exports = shortestWord;
